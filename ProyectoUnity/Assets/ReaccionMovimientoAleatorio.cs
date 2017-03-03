aa4ea8af9a1cfc7a9fe26a03217bcca6dfaabb2e
Henry <vacarapi@gmail.com>
2017-03-03 17:19:27 -0600

    El resto del proyecto

---
+++ ProyectoUnity/Assets/ReaccionMovimientoAleatorio.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReaccionMovimientoAleatorio : ReaccionQuimica
+{
+    public float Velocidad = 0.1f;
+    public float RotMax = 50;
+    Vector2 pos;
+
+	// Use this for initialization
+	void Start () {
+        pos = new Vector2(0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (reaccionando)
+        {
+            tiempoRestante = tiempoRestante - Time.deltaTime;
+            
+            pos = pos + new Vector2(1, 1).normalized * Velocidad;
+
+            float p = Mathf.PerlinNoise(pos.x, pos.y);
+
+            transform.rotation = Quaternion.Euler(0, p * RotMax, p * RotMax);
+
+            if (tiempoRestante <= 0)
+            {
+                reaccionando = false;
+            }
+        }
+    }
+
+
+
+    public override void Reaccionar()
+    {
+        reaccionando = true;
+        tiempoRestante = TiempoReaccion;
+    }
+}
---
+++ ProyectoUnity/Assets/UIExperimentos.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIExperimentos : MonoBehaviour 
+{
+	public Text TextoAgarrado;
+	public Text TextoCantidadAgarrado;
+	public GameObject SinAgarrar;
+
+	public Text TextoNombreReactivo;
+	public Text TextoCantidadReactivo;
+	public Text TextoTiempoReaccion;
+    public Text TextoCantidadAcidoRestante;
+    public Text TextoAzufreRestante;
+	public GameObject SinReactivo;
+
+	void Start ()
+	{
+		MostrarAgarrable (false);
+		MostrarReactivo (false);
+	}
+	
+	void Update () {
+		
+	}
+
+
+	public void MostrarInformacion(MaterialAgarrable m) 
+	{
+		MostrarAgarrable (true);
+
+		TextoAgarrado.text = m.NombreMaterial;
+		TextoCantidadAgarrado.text = m.CantidadFormateada;
+	}
+
+
+	public void MostrarInformacionReactivo(MaterialReactivo reactivo)
+	{
+		MostrarReactivo (true);
+
+		TextoNombreReactivo.text = reactivo.NombreReactivo;
+		TextoCantidadReactivo.text = reactivo.CantidadFormateada;
+	}
+
+
+
+
+
+	public void MostrarAgarrable(bool mostrar)
+	{
+		TextoAgarrado.transform.parent.gameObject.SetActive (mostrar);
+		TextoCantidadAgarrado.transform.parent.gameObject.SetActive (mostrar);
+		SinAgarrar.gameObject.SetActive (!mostrar);
+	}
+
+
+	public void MostrarReactivo(bool mostrar)
+	{
+		TextoNombreReactivo.transform.parent.gameObject.SetActive (mostrar);
+		TextoCantidadReactivo.transform.parent.gameObject.SetActive (mostrar);
+        SinReactivo.SetActive (!mostrar);
+
+		if (!mostrar) {
+			TextoTiempoReaccion.transform.parent.gameObject.SetActive (false);
+            TextoAzufreRestante.transform.parent.gameObject.SetActive(false);
+            TextoCantidadAcidoRestante.transform.parent.gameObject.SetActive(false);
+		}
+	}
+
+
+	public void ActualizarDatosReactivo(MaterialReactivo reactivo, float tiempo)
+	{
+		TextoTiempoReaccion.transform.parent.gameObject.SetActive (true);
+        TextoAzufreRestante.transform.parent.gameObject.SetActive(true);
+        TextoCantidadAcidoRestante.transform.parent.gameObject.SetActive(true);
+        TextoTiempoReaccion.text = FormatearTiempo (tiempo);
+
+        ReactivoAcidoClorhidrico acido = (ReactivoAcidoClorhidrico)reactivo;
+
+        float acidoRestante = acido.CalcularAcidoRestante(tiempo);
+        float azufreRestante = acido.CalcularAzufreRestante(tiempo);
+
+        string acidoFormateado = MaterialReactivo.FormatearCantidad(acidoRestante, MaterialReactivo.UnidadMedida.ml);
+        string azufreFormateado = MaterialReactivo.FormatearCantidad(azufreRestante, MaterialReactivo.UnidadMedida.gr);
+
+        TextoCantidadAcidoRestante.text = acidoFormateado;
+        TextoAzufreRestante.text = azufreFormateado;
+    }
+
+
+
+
+    private string FormatearTiempo(float tiempo)
+	{
+		int mins = (int)(tiempo/60);
+		float segs = (((int)tiempo) % 60) + (((tiempo*1000)%1000)/1000f);
+
+		string res = "";
+		if (mins > 0) {
+			string m = mins > 1 ? "mins" : "min";
+			res = res + mins + " " + m + ", ";
+		}
+
+		res = res + string.Format("{0:0.00}",segs) + " segs";
+
+		return res;
+	}
+}
