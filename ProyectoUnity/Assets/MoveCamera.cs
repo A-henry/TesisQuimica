f455f44ed89b0434b280617755a0dec4b17dbb5c
Henry <vacarapi@gmail.com>
2017-03-10 18:38:45 -0600

    Cambios en la escena de Estante

---
+++ ProyectoUnity/Assets/MoveCamera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MoveCamera : MonoBehaviour
+{
+
+
+    public float velocidadDespzamiento ;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+       
+        if(Input.GetMouseButton(1))
+        {
+            float pointer_y = Input.GetAxis("Mouse X");
+            float pointer_x = Input.GetAxis("Mouse Y");
+            gameObject.transform.Rotate(0, pointer_y * 2, 0);
+
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(Vector3.forward * velocidadDespzamiento);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(Vector3.forward * -velocidadDespzamiento);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(Vector3.left * velocidadDespzamiento);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(Vector3.right * velocidadDespzamiento);
+        }
+    }
+}
